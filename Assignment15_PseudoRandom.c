e929d3fda0cad55e0836650291c0ec238d6d8d95
arhamg22 <arham22kk@gmail.com>
2026-04-02 07:37:40 +0530

    Update Assignment15_PseudoRandom.c

--- Assignment15_PseudoRandom.c
+++ Assignment15_PseudoRandom.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-int main() {
+#include <stdlib.h>  
+#include <time.h>     
 
-    int random_number = rand();
-    printf("Generated Pseudo Random Number: %d\n", random_number);
+int main() {
+    int n, i;
+
+    // Input: how many random numbers
+    printf("Enter how many random numbers you want: ");
+    scanf("%d", &n);
+    srand(time(0));
+
+    printf("Pseudo Random Numbers:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d\n", rand());
+    }
 
     return 0;
 }
