9acb804af29cd8b75693706fe3502f8ba82704ab
arhamg22 <arham22kk@gmail.com>
2026-04-03 05:18:22 +0000

    save

---
+++ Assignment19_employee.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+struct EMPLOYEE {
+    char name[50];
+    char designation[50];
+    char gender;
+    int date_of_joining;
+    double salary;
+};
+
+int main() {
+    
