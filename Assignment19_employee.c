3c5c43f3a7ad75e8d5ab54c3e9d7b1ca14844637
arhamg22 <arham22kk@gmail.com>
2026-04-03 05:19:00 +0000

    save

--- Assignment19_employee.c
+++ Assignment19_employee.c
@@ -10,4 +10,46 @@ struct EMPLOYEE {
 };
 
 int main() {
-    
+      struct EMPLOYEE employees[100];
+    int n;
+    printf("Enter the number of employees: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter details for employee %d:\n", i + 1);
+        printf("Name: ");
+        scanf("%s", employees[i].name);
+        printf("Designation: ");
+        scanf("%s", employees[i].designation);
+        printf("Gender (M/F): ");
+        scanf(" %c", &employees[i].gender);
+        printf("Date of Joining (YYYYMMDD): ");
+        scanf("%d", &employees[i].date_of_joining);
+        printf("Salary: ");
+        scanf("%lf", &employees[i].salary);
+    }
+     printf("\nTotal number of employees: %d\n", n);
+     int male_count = 0, female_count = 0;
+     for (int i = 0; i < n; i++) {
+        if (employees[i].gender == 'M' || employees[i].gender == 'm') {
+            male_count++;
+        } else {
+            female_count++;
+        }
+         printf("Count of male employees: %d\n", male_count);
+         printf("Count of female employees: %d\n", female_count);
+          printf("\nEmployees with salary more than 10k:\n");
+    for (int i = 0; i < n; i++) {
+        if (employees[i].salary > 10000) {
+            printf("Name: %s, Salary: %.2lf\n", employees[i].name, employees[i].salary);
+        }
+    }
+    printf("\nEmployees with designation 'Assistant Manager':\n");
+    for (int i = 0; i < n; i++) {
+        if (strcmp(employees[i].designation, "Assistant Manager") == 0) {
+            printf("Name: %s, Salary: %.2lf\n", employees[i].name, employees[i].salary);
+        }
+    }
+
+    return 0;
+}
