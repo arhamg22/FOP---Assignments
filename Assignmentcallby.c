40663583b94c1d8b9dcf3cd2738d4a5afd1fa651
arhamg22 <arham22kk@gmail.com>
2026-04-09 06:16:38 +0000

    save

---
+++ Assignmentcallby.c
@@ -0,0 +1,19 @@
+#inlcude <stdio.h>
+void swap_value(int a, int b){
+    int temp = a;
+    a = b;
+    b = temp;
+
+}
+void swap_ref(int*a,int*b){
+
+     int temp=*a;
+     *b = temp;
+}
+void main(){
+    int a,b;
+    int ch;
+    printf("Enter a:");
+    scanf("%d", &a);
+    
+}
